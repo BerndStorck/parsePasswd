dee44c97e1099784962b307da66a884137714a77
Bernd Storck <berndstorck@hotmail.com>
2023-11-23 17:52:27 +0100

    2 not nedded variables deleted. Optimization in one funktion.

--- chk1char.sh
+++ chk1char.sh
@@ -2,8 +2,8 @@
 #
 # chk1char.sh
 #
-# Version:    1.0.0
-# History:    2023-11-21
+# Version:    1.0.2
+# History:    2023-11-21, 2023-11-23
 #
 # Project Description:
 #
@@ -20,31 +20,34 @@ readonly WHITE_BOLD='\e[1;37m'
 readonly YELLOW_BOLD_INTENS='\e[1;93m'
 readonly COLOROFF='\e[0m'
 
+file2check="/etc/passwd"
+if [ -n "$1" ] && [ -f "$1" ]; then
+  file2check="$1"
+fi
+
 install_missed_program () {
   local wanted_program
   local is_installed=false
-  
+
   if [ -z "$1" ]; then
     return 1
   fi
-  
+
   wanted_program="$1"
-  
+
   if ! command -v "$wanted_program" > /dev/null 2>&1; then
     printf "Das Programm '%b%b%b' wird benötigt!\n" "$WHITE_BOLD" "$wanted_program" "$COLOROFF"
-    for pmanager in "apt" "aptitude" "apt_get"; do
-      if command -v "$pmanager" > /dev/null 2>&1; then
-        if ! sudo -vn > /dev/null 2>&1; then
-          break
-        fi      
-        if sudo "$pmanager" install "$wanted_program"; then
-          is_installed=true
-        fi
-        break
+    pmanager=$(command -v apt aptitude apt-get | head -n 1)
+    if [ -n "$pmanager" ]; then
+      if ! sudo -vn > /dev/null 2>&1; then  # Der Anwender ist kein Sudoer
+        return 1
       fi
-    done
+      if sudo "$pmanager" install "$wanted_program"; then
+        is_installed=true
+      fi
+    fi
     if ! "$is_installed"; then
-      printf "WARNING: You have to install '%b%b%b' for full functionality!\n" "${WHITE_BOLD}" "$wanted_program" "${COLOROFF}"
+      printf "WARNING: You have to install '%b%b%b' for full functionality!\n" "$WHITE_BOLD" "$wanted_program" "$COLOROFF"
     fi
   fi
 }
@@ -54,12 +57,11 @@ find_non_alpha_start () {
   local answer
 
   [ ! "$1" ] && data_file="/etc/passwd" || data_file="$1"
-  
+
   while IFS=: read -r accountname rest; do
     first_char="${accountname:0:1}"
     if [[ ! $first_char =~ [[:alpha:]] ]]; then
         answer+="$accountname: $rest\n"
-        found=true
     fi
   done < "$data_file"
   echo "$answer"
@@ -67,21 +69,20 @@ find_non_alpha_start () {
 
 
 line_with_character_description () {
-  local answer
-  printf "${WHITE_BOLD}%s\n${COLOROFF}%s\n" "$1" "$(ascii "${1:0:1}" | head -n 1)"
+  printf "${WHITE_BOLD}%b${COLOROFF}%s\n" "$1" "$(ascii "${1:0:1}" | head -n 1)"
 }
 
 install_missed_program "ascii"
 
-lines="$(find_non_alpha_start)"
+lines="$(find_non_alpha_start "$file2check")"
 if [ -n "$lines" ]; then
   if command -v ascii > /dev/null 2>&1; then
-    printf "${YELLOW_BOLD_INTENS}%s${COLOROFF}\n\n" "Folgende Zeilen beginnen nicht mit einem Buchstaben:"
+    printf "${YELLOW_BOLD_INTENS}%s${COLOROFF}\n\n" "Folgende Zeilen aus '$file2check' beginnen nicht mit einem Buchstaben:"
     while read -r line; do
       line_with_character_description "$line"
     done <<< "$lines"
   else
-    printf "Folgende Zeilen beginnen nicht mit einem Buchstaben:\n\n%s\n" "$lines"
+    printf "Folgende Zeilen aus '$file2check' beginnen nicht mit einem Buchstaben:\n\n%s\n" "$lines"
   fi
 fi
 
